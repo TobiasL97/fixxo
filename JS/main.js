4896b879420acb64cd7b3a8304fb51ad0671bc96
Tobias <tobbe-l@outlook.com>
2022-10-09 23:28:22 +0200

    added animation to hamburger-icon

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,18 @@
-window.onload = function () {
+// Sliding menu for mobile 
 
-    const menu_btn = document.querySelector('.navbar-menu')
-    const mobile_menu = document.querySelector('.hamburger-menu');
+const hamburger_menu = document.querySelector('.hamburger-menu');
+const hamburger_icon = document.querySelector('.hamburger-icon');
 
-    menu_btn.addEventListener('click', function () {
-        mobile_menu.classList.add('is-active')
-    });
+const mobileMenu = () => {
+    if (hamburger_icon.classList.contains('is-active')) //close hamburger-menu
+    {
+        hamburger_menu.classList.remove('is-active');
+        hamburger_icon.classList.remove('is-active');
+    }
+    else { //open hamburger-menu
+        hamburger_menu.classList.add('is-active');
+        hamburger_icon.classList.add('is-active');
+        
+    }
+    
 }
